71dba846d852b5b3da633e74fe63c9f1166a3bc0
N. Squidd <notsquidd@gmail.com>
2016-01-16 22:11:39 -0700

    Encapsulate NHentai into Hentai

--- lib/hentai/nhentai.rb
+++ lib/hentai/nhentai.rb
@@ -3,112 +3,114 @@ require 'nokogiri'
 require 'rest-client'
 require 'json'
 
-module NHentai
+module Hentai
+  module NHentai
 
-  # Constants
-  BASE_URL = "http://nhentai.net"
-  IMAGE_BASE_URL = "http://i.nhentai.net"
-  THUMB_BASE_URL = "http://t.nhentai.net"
+    # Constants
+    BASE_URL = "http://nhentai.net"
+    IMAGE_BASE_URL = "http://i.nhentai.net"
+    THUMB_BASE_URL = "http://t.nhentai.net"
 
-  # Class defs
+    # Class defs
 
-  class Doujin
-    attr_accessor :upload_date, :num_favorites, :media_id, :title, :cover,
-                  :pages, :language, :artist, :tags, :group, :parody, 
-                  :category, :scanlator, :id, :num_pages
+    class Doujin
+      attr_accessor :upload_date, :num_favorites, :media_id, :title, :cover,
+                    :pages, :language, :artist, :tags, :group, :parody, 
+                    :category, :scanlator, :id, :num_pages
 
-    def initialize(hash)
-      @upload_date = hash["upload_date"]
-      @num_favorites = hash["num_favorites"]
-      @media_id = hash["media_id"]
-      @title = Title.new hash["title"]
-      @cover = Picture.new hash["images"]["cover"], 
-              THUMB_BASE_URL + "/galleries/" + media_id + "/thumb.jpg"
-      @pages = get_pages hash["images"]["pages"], media_id
+      def initialize(hash)
+        @upload_date = hash["upload_date"]
+        @num_favorites = hash["num_favorites"]
+        @media_id = hash["media_id"]
+        @title = Title.new hash["title"]
+        @cover = Picture.new hash["images"]["cover"], 
+                THUMB_BASE_URL + "/galleries/" + media_id + "/thumb.jpg"
+        @pages = get_pages hash["images"]["pages"], media_id
 
-      # Languae, artist, characters, category, tags, group, parody, and possibly
-      # others are treated as "tags" in the shitty JSON API. Each of these
-      # attributes can have multiple values. Need to figure out a good way to
-      # handle this.
+        # Languae, artist, characters, category, tags, group, parody, and possibly
+        # others are treated as "tags" in the shitty JSON API. Each of these
+        # attributes can have multiple values. Need to figure out a good way to
+        # handle this.
 
-      @id = hash["id"]
-      @num_pages = hash["num_pages"]
-    end
-
-    private
-      def get_pages(images, media_id)
-        pages = []
-        images.each_with_index do |image, index|
-          # XXX: check image type
-          pages.push Picture.new image, 
-                    IMAGE_BASE_URL + "/galleries/" + media_id + 
-                    "/" + (index+1).to_s + ".jpg"
-        end
-
-        pages
+        @id = hash["id"]
+        @num_pages = hash["num_pages"]
       end
 
-  end
+      private
+        def get_pages(images, media_id)
+          pages = []
+          images.each_with_index do |image, index|
+            # XXX: check image type
+            pages.push Picture.new image, 
+                      IMAGE_BASE_URL + "/galleries/" + media_id + 
+                      "/" + (index+1).to_s + ".jpg"
+          end
 
-  class Title
-    attr_accessor :english, :japanese
+          pages
+        end
 
-    def initialize(hash)
-      @english = hash["english"]
-      @japanese = hash["japanese"]
     end
 
-  end
+    class Title
+      attr_accessor :english, :japanese
 
-  class Picture
-    attr_accessor :height, :width, :type, :url
+      def initialize(hash)
+        @english = hash["english"]
+        @japanese = hash["japanese"]
+      end
 
-    def initialize(hash, url)
-      @height = hash["h"]
-      @width = hash["w"]
-      @type = hash["t"]
-      @url = url
     end
-  end
 
-  # Module methods
-  def NHentai.search(options = {})
-    tag = options.fetch :tag
+    class Picture
+      attr_accessor :height, :width, :type, :url
 
-    # open url
-    page = Nokogiri::HTML(open(BASE_URL + "/tag/" + tag))
+      def initialize(hash, url)
+        @height = hash["h"]
+        @width = hash["w"]
+        @type = hash["t"]
+        @url = url
+      end
+    end
 
-    # we first need to all doujins on a page. they look like this
-    #
-    #    <div class="gallery" data-tags="8010 10988 22942 29013">
-    #        <a href="/g/154483/" class="cover">
-    #            <img src="//t.nhentai.net/galleries/894501/thumb.jpg" width="250" height="359" alt="..." />
-    #            <div class="caption">[Anthology] LQ -Little Queen- Vol. 6 [Digital]</div>
-    #        </a>
-    #    </div>
-    #
-    # We need to gather all hrefs to do further digging.
-    # Therefore, we should grab the href with class of "cover"
+    # Module methods
+    def NHentai.search(options = {})
+      tag = options.fetch :tag
 
-    doujins = []
+      # open url
+      page = Nokogiri::HTML(open(BASE_URL + "/tag/" + tag))
 
-    page.css(".cover").each do |link|
-
-      # Now that we have a link, we need to follow "BASE_URL + link + json" 
-      # to access the bad API
+      # we first need to all doujins on a page. they look like this
       #
-      # `link' looks like: /g/154427/
+      #    <div class="gallery" data-tags="8010 10988 22942 29013">
+      #        <a href="/g/154483/" class="cover">
+      #            <img src="//t.nhentai.net/galleries/894501/thumb.jpg" width="250" height="359" alt="..." />
+      #            <div class="caption">[Anthology] LQ -Little Queen- Vol. 6 [Digital]</div>
+      #        </a>
+      #    </div>
+      #
+      # We need to gather all hrefs to do further digging.
+      # Therefore, we should grab the href with class of "cover"
 
-      json = JSON.parse(RestClient.get(BASE_URL + link.attribute("href") + "json"))
-      
-      doujin = Doujin.new(json)
+      doujins = []
 
-      doujins.push doujin
+      page.css(".cover").each do |link|
+
+        # Now that we have a link, we need to follow "BASE_URL + link + json" 
+        # to access the bad API
+        #
+        # `link' looks like: /g/154427/
+
+        json = JSON.parse(RestClient.get(BASE_URL + link.attribute("href") + "json"))
+        
+        doujin = Doujin.new(json)
+
+        doujins.push doujin
+
+      end
+
+     doujins 
 
     end
 
-   doujins 
-
   end
-
 end
